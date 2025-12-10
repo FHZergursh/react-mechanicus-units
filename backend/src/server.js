be43b0f6e965c441310ae9c9c421b1c51b5ab6c3
Flinn Alexnader Hancock <89704618+FHZergursh@users.noreply.github.com>
2025-12-10 16:15:36 +0000

    scope models, setup weapon route apis
    both aren't complete, just an early stage to establish functionality

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 import express from "express"
+import weaponRoute from './routes/weapon.route.js'
 
 
 const app = express()
 const PORT = 3005;
 
+app.use(express.json())
 
+app.use("/weapons", weaponRoute)
 
 app.listen(PORT, console.log("server started on port " + PORT))
